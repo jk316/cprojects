0bc5785a8e7aa37a6c19a6f23deba4f8094a8efd
jk316 <45003786+jk316@users.noreply.github.com>
2021-07-28 20:28:52 +0800

    add a func named showMenu

---
+++ first/test.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+// show menu
+void showMenu(){
+
+    cout<<"1：添加联系人"<<endl;
+    cout<<"2：显示联系人"<<endl;
+    cout<<"3：显示联系人"<<endl;
+    cout<<"4：显示联系人"<<endl;
+    cout<<"5：显示联系人"<<endl;
+    cout<<"6：显示联系人"<<endl;
+    cout<<"7：显示联系人"<<endl;
+    
+}
+
+
+
+
+int main(){
+    
+    showMenu();
+    
+    system("pause");
+    return 0;
+}
+
